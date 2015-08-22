8b75f49d912346f95c760e07ac7c894943ab92e8
bjornstahl <arcan-fe@nospam>
2015-08-22 11:58:02 +0200

    label propagation / mapping work

--- durden/atypes/terminal.lua
+++ durden/atypes/terminal.lua
@@ -2,17 +2,8 @@
 -- Terminal archetype, settings and menus specific for terminal-
 -- frameserver session (e.g. keymapping, state control)
 --
-local symmap = {
-	LEFT = "LEFT",
-	RIGHT = "RIGHT",
-	UP = "UP",
-	DOWN = "DOWN",
-	PAGEUP = "SCROLL_UP",
-	PAGEDOWN = "SCROLL_DOWN"
-};
-
 return {
+	dispatch = {},
 	actions = {},
-	labels = symmap,
 	atype = "terminal"
 };
