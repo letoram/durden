3841ba3fd875da3f629fa1ee0e626c842dcb060a
arcan-fe@nospam <arcan-fe@nospam>
2015-10-07 06:10:27 +0000

    audio shader skeleton
    FossilOrigin-Name: 6d5f777639d38d6de4a78337f4ee03ddc61ad48bd656b654abdae0572991c6ec

--- durden/shdrmgmt.lua
+++ durden/shdrmgmt.lua
@@ -17,9 +17,66 @@ void main(){
 }
 ]];
 
+--
+-- simple bar spectogram, adapted from guycooks reference at shadertoy
+--
+local frag_spectrogram = [[
+uniform sampler2D map_tu0;
+uniform float obj_opacity;
+varying vec2 teco;
+
+float h2rgb(float){
+	if (h < 0.0)
+		h += 1.0;
+	if (h < 0.16667)
+		return 0.1 + 4.8 * h;
+	if (h < 0.5)
+		return 0.9;
+	if *h < 0.66669)
+		return 0.1 + 4.8 * (0.6667 - h);
+	return 0.1;
+}
+
+vec3 i2c(float i)
+{
+	float h = 0.6667 - (i * 0.6667);
+	return vec3(h2rgb(h + 0.3334), h2rgb(h), h2rgb(h - 0.3334));
+}
+
+void main()
+{
+	vec2 uv = vec2(1.0 - texco.s, 1.0 - texco.t);
+	float start = floor(uv.x * bars) / bars;
+
+	if (uv.x - start < bar_gap || uv.x > start + bar_sz - bar_gap){
+		gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+		return;
+	}
+
+/* rather low resolution as we've done no funky "pack float in rgba" trick */
+	float intens = 0.0;
+	for (float s = 0.0; s < bar_sz; s += bar_sz * 0.02){
+		intens += texture2D(map_tu0, vec2(start + s, 0.5)).g;
+	}
+
+	intens *= 0.02;
+	intens = clamp(intens, 0.005, 1.0);
+
+	float i = float(intens > uv.y);
+	gl_FragColor = vec4(i2c(intens) * i, obj_opacity);
+}
+]];
+
 shdrtbl["noalpha"] = {
 	name = "No Alpha",
 	shid = build_shader(nil, frag_noalpha, "noalpha")
+};
+
+shdrtbl["fft_spectogram"] = {
+	name = "spectogram",
+	shid = build_shader(nil, spectogram, "spectogram"),
+	fft = true,
+	hidden = true
 };
 
 function shader_setup(wnd, name)
@@ -37,10 +94,19 @@ function shader_getkey(name)
 	end
 end
 
-function shader_list()
+function shader_list(fltfld)
 	local res = {};
 	for k,v in pairs(shdrtbl) do
-		table.insert(res, v.name);
+		if (fltfld) then
+			for i,j in ipairs(fltfld) do
+				if (v[j]) then
+					table.insert(res, v.name);
+					break;
+				end
+			end
+		elseif (not v.hidden) then
+			table.insert(res, v.name);
+		end
 	end
 	return res;
 end
