2073b0185b16b03c5b7cf92fb9fb5e996b3fdbad
arcan-fe@nospam <arcan-fe@nospam>
2017-12-24 23:36:01 +0000

    particle system flair updates
    still is quite a ways from showing the snow effect to a level better
    than xsnow, but it's getting there :-)
    
    FossilOrigin-Name: d7fda44cffafdbcd1aa65bc4a9552ce787f45d70674ef5cba3dd6376a77a9209

--- durden/tools/flair/psys.lua
+++ durden/tools/flair/psys.lua
@@ -1,4 +1,5 @@
 -- simple particle system generator
+--
 -- initiated with a table of possible callbacks:
 --  create(vid, state) : build surface, seed state, let system tag
 --                       vid is set to a linked null surface at starting
@@ -15,39 +16,43 @@ local psys_list = {};
 
 local function psys_create(v)
 	local nsrf = null_surface(
-		math.random(v.particle_min_w, v.particle_max_w),
-		math.random(v.particle_min_h, v.particle_max_h)
+		math.random(v.emitter.min_w, v.emitter.max_w),
+		math.random(v.emitter.min_h, v.emitter.max_h)
 	);
 	show_image(nsrf);
 	link_image(nsrf, v.anchor);
 
 -- set offset and set direction based on emitter geometry
-	local dir = v:apply_emitter(nsrf);
+	if (v.emitter.shape == "rectangle") then
+		move_image(nsrf,
+			math.random(v.emitter.x1, v.emitter.x2),
+			math.random(v.emitter.y1, v.emitter.y2)
+		);
+	end
+
 	local iv = {
-		direction = dir,
-		velocity = math.random(v.particle_min_vel, v.particle_max_vel),
 		vid = nsrf
 	};
 
--- either lifespan or when it goes off screen
--- (dynamic systems should treat the screen as a hard boundary)
-	if (v.lifespan) then
-		expire_image(v.vid, v.lifespan);
-	end
+	v.create(iv);
+	table.insert(v.particles, iv);
 end
 
 local function psys_step()
 	for _,v in ipairs(psys_list) do
 		for _,p in ipairs(v.particles) do
-			v.update(p.vid, p.state, 1);
+			v.update(p);
 		end
 	if (v.counter > 0) then
 		v.counter = v.counter - 1;
 	else
 -- try emit
-		if (v.particle_count < v.particle_limit) then
+		if (#v.particles < v.particle_limit) then
 			psys_create(v);
 			v.counter = v.counter_start;
+		else
+			local p = table.remove(v.particles, 1);
+			v.destroy(p, 5);
 		end
 	end
 	end
@@ -55,6 +60,16 @@ end
 
 return function(handlers, opts)
 	local res = {};
+	if (not handlers or not handlers.create or not handlers.update) then
+		warning("flair/psys - invalid particle system");
+		return;
+	end
+
+	if (not opts) then
+		opts = {};
+	end
+
+	local display = active_display();
 
 -- defer timer registration until first system is set, and use a shared timer
 	if (not in_psys) then
@@ -62,6 +77,33 @@ return function(handlers, opts)
 		in_psys = true;
 	end
 
+	local new_sys = {
+		create = handlers.create,
+		update = handlers.update,
+		destroy = handlers.destroy,
+		display = display,
+		counter_start = 1,
+		counter = 1,
+		particle_limit = opts.particle_limit and opts.particle_limit or 100,
+		anchor = null_surface(1, 1),
+		particles = {}
+	};
+
+	show_image(new_sys.anchor);
+	link_image(new_sys.anchor, display.order_anchor);
+
+	if (not opts.emitter) then
+		new_sys.emitter = {
+			shape = "rectangle",
+			x1 = 0, y1 = 0,
+			x2 = math.random(1, display.width) - 1, y2 = 1,
+			min_w = 1, max_w = 4, min_h = 1, max_h = 4,
+			dx = {-1, 1},
+			dy = {0, 1}
+		}
+	else
+		new_sys.emitter = opts.emitter;
+	end
+
 	table.insert(psys_list, new_sys);
--- opts: initial seed counter, upper limit, distribution function/method, ..
 end
--- durden/tools/flair/snow.lua
+++ durden/tools/flair/snow.lua
@@ -1,24 +1,37 @@
 -- particle rules simulating falling snow
 --
--- emission:
--- set direction to +y
--- apply force (wind), distance and 1.0 / 1.0 + dist.
--- integration:
---   x += sx, y += sy
---  sx += ax, sy += ay
---
---
 
-local function snow_create(vid, state)
-	print("time to create");
+local white = fill_surface(4, 4, 255, 255, 255);
+
+local function snow_create(state)
+	image_sharestorage(white, state.vid);
+	blend_image(state.vid, 0.5 + 0.5 * math.random());
+	rotate_image(state.vid, math.random(0, 359));
+	state.velocity_x = 0;
+	state.velocity_y = 0;
+	state.acceleration_x = 0;
+	state.acceleration_y = 0.1;
 end
 
-local function snow_update(vid, state, tickcount)
-	print("time to step");
+local function snow_update(state)
+	nudge_image(state.vid, state.velocity_x, state.velocity_y);
+	state.velocity_x = state.velocity_x + state.acceleration_x;
+	state.velocity_y = state.velocity_y + state.acceleration_y;
+end
+
+local function snow_step(state)
+-- vary the global wind
+	state.force_x = math.pow(math.sin(counter) * 0.5 + 0.5, 4) * 150;
+	state.force_y = math.sin(counter * 100) * 20;
+end
+
+local function snow_destroy(state)
+	delete_image(state.vid);
 end
 
 return {
 	create = snow_create,
 	update = snow_update,
-	destroy = snow_destroy
+	destroy = snow_destroy,
+	step = snow_step
 };
