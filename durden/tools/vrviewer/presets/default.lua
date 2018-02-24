d3a7c1a173ea238d01d7da1ce39ba536941eaf77
bjornstahl <arcanfe@nospam>
2018-02-25 00:08:55 +0100

    numerous changes to display-lease/vr management
    this should fix all the remaining problems with cleaning up the
    vr-setup for safe reactivation so the main process doesn't need
    to be shut down between runs.

--- durden/tools/vrviewer/presets/default.lua
+++ durden/tools/vrviewer/presets/default.lua
@@ -4,7 +4,7 @@ return {
 "layers/layer_bg/settings/fixed=true",
 "layers/layer_bg/settings/ignore=true",
 "layers/layer_bg/add_model/sphere=bg",
-"layers/layer_bg/models/bg/source=images/skymap.png",
+"layers/layer_bg/models/bg/source=sky_photo4.jpg",
 "layers/layer_bg/models/bg/connpoint=vrbg",
 "layers/add=fg",
 "layers/layer_fg/terminal"
