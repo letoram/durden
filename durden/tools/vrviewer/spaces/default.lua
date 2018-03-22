bc7c2ce55b5f5437377c315855ee74f90c856a35
arcan-fe@nospam <arcan-fe@nospam>
2018-03-22 20:29:05 +0000

    (vrviewer) add child-swap, more menu controls
    FossilOrigin-Name: 2cd58efa2b70b00eb4a24ff0c9ed40643564978bcb65127c658a47903418b6c3

--- durden/tools/vrviewer/spaces/default.lua
+++ durden/tools/vrviewer/spaces/default.lua
@@ -1,22 +1,37 @@
 return {
 -- background, can't be swapped in and at the end of the viewport
 "layers/add=bg",
-"layers/layer_bg/settings/depth=99.0",
-"layers/layer_bg/settings/radius=99.0",
+"layers/layer_bg/settings/depth=50.0",
+"layers/layer_bg/settings/radius=50.0",
 "layers/layer_bg/settings/fixed=true",
 "layers/layer_bg/settings/ignore=true",
-"layers/layer_bg/add_model/sphere=bg",
-"layers/layer_bg/models/bg/source=park.png",
 
--- allow a temporary override of the source using the 360bg connpoint
-"layers/layer_bg/models/bg/connpoint/temporary=360bg",
+-- a little extra work to define a multi- mapped textured cube
+"layers/layer_bg/add_model/cube=bg",
+"layers/layer_bg/models/bg/multimap",
+"layers/layer_bg/models/bg/faces/1/source=box/0.png", -- +x
+"layers/layer_bg/models/bg/faces/2/source=box/1.png", -- -x
+"layers/layer_bg/models/bg/faces/3/source=box/2.png", -- +y
+"layers/layer_bg/models/bg/faces/4/source=box/3.png", -- -y
+"layers/layer_bg/models/bg/faces/5/source=box/4.png", -- +z
+"layers/layer_bg/models/bg/faces/6/source=box/5.png", -- -z
 
 -- an interactive foreground layer with a transparent terminal
 "layers/add=fg",
+"layers/layer_fg/settings/active_scale=2",
+"layers/layer_fg/settings/inactive_scale=1",
+"layers/layer_fg/settings/depth=1.0",
+"layers/layer_fg/settings/radius=10.0",
+"layers/layer_fg/settings/spacing=0.2",
 "layers/layer_fg/terminal=bgalpha=128",
 
--- and a hidden model that only gets activated on client connect/disconnect and uses side by side
+-- a hidden model that only gets activated on client
+-- connect/disconnect and uses side by side
 --"layers/layer_fg/add_model/rectangle=sbsvid",
 --"layers/layer_fg/models/sbsvid/connpoint/reveal=sbsvid",
 --"layers/layer_fg/models/sbsvid/stereoscopic=sbs"
 };
+
+-- "layers/layer_bg/add_model/sphere=bg",
+-- "layers/layer_bg/models/bg/source=center360.png",
+-- "layers/layer_bg/models/bg/connpoint/temporary=360bg",
