40addf16ef99d841457ae65a1146072b39ae825f
bjornstahl <arcanfe@nospam>
2018-08-31 01:27:04 +0200

    visual menu split up into multiple subs

--- durden/menus/global/visual.lua
+++ durden/menus/global/visual.lua
@@ -11,130 +11,7 @@ for k,v in pairs(hint_lut) do
 	hint_rlut[v] = k;
 end
 
-local function run_group(group, prefix, wnd)
-	local ad = active_display();
-	local as = ad.selected;
-
--- hack around the problem with many menu paths written using the dumb
--- active_display().selected
-	if (wnd) then
-		ad.selected = wnd;
-	end
-
--- this doesn't check / account for reattachments/migrations etc.
-	if (group and type(group) == "table") then
-		for k,v in ipairs(group) do
-			if (type(v) == "string" and string.starts_with(v, prefix)) then
-				dispatch_symbol(v);
-			end
-		end
-	end
-
--- and restore if we didn't destroy something, but this method isn't safe
--- from modification, and that's mentioned in the documentation
-	if (as and as.canvas) then
-		ad.selected = as;
-	end
-end
-
-local function run_domain(group, pal, set)
-	run_group(group, "/global/", nil);
-	if (set) then
--- need a copy to survive UAF- self modification
-		local lst = {};
-		for k,v in ipairs(set) do
-			table.insert(lst, v);
-		end
-		for i,wnd in ipairs(lst) do
-			if (wnd.canvas) then
-				run_group(group, "/target/", wnd);
-			end
-			apply_scheme(pal, wnd);
-		end
-	end
-end
-
-local function tryload_scheme(v)
-	local res = system_load(v, 0);
-	if (not res) then
-		warning(string.format("devmaps/schemes, system_load on %s failed", v));
-		return;
-	end
-
-	local okstate, tbl = pcall(res);
-	if (not okstate) then
-		warning(string.format("devmaps/schemes, couldn't parse/extract %s", v));
-		return;
-	end
-
--- FIXME: [a_Z,0-9 on name]
-	if (type(tbl) ~= "table" or not tbl.name or not tbl.label) then
-		warning(string.format("devmaps/schemes, no name/label field for %s", v));
-		return;
-	end
-
--- pretty much all fields are optional as it stands
-	return tbl;
-end
-
-local schemes;
-local function scan_schemes()
-	schemes = {};
-	local list = glob_resource("devmaps/schemes/*.lua", APPL_RESOURCE);
-	for i,v in ipairs(list) do
-		local res = tryload_scheme("devmaps/schemes/" .. v);
-		if (res) then
-			table.insert(schemes, res);
-		end
-	end
-end
-
-
-function ui_scheme_menu(scope, tgt)
-	local res = {};
-	if (not schemes) then
-		scan_schemes();
-		if (not schemes) then
-			return;
-		end
-	end
-
-	for k,v in ipairs(schemes) do
-		table.insert(res, {
-			name = v.name,
-			label = v.label,
-			kind = "action",
-			handler = function()
-				if (scope == "global") then
-					local lst = {};
-					for wnd in all_windows(true) do
-						table.insert(lst, wnd);
-					end
-					run_domain(v.actions, v.palette, lst);
-				elseif (scope == "display") then
-					local lst = {};
-					for i, wnd in ipairs(tgt.windows) do
-						table.insert(lst, wnd);
-					end
-					run_domain(v.actions, nil, lst);
-					run_domain(v.display, v.palette, lst);
-				elseif (scope == "workspace") then
-					local lst = {};
-					for i,v in ipairs(tgt.children) do
-						table.insert(lst, v);
-					end
-					run_domain(v.actions, nil, lst);
-					run_domain(v.workspace, v.palette, lst);
-				elseif (scope == "window") then
-					run_domain(v.actions, nil, {tgt});
-					run_domain(v.window, v.palette, {tgt});
-				end
-			end
-		});
-	end
-
-	return res;
-end
+system_load("menus/global/schemes.lua")();
 
 local durden_font = {
 	{
@@ -191,81 +68,6 @@ local durden_font = {
 	}
 };
 
--- (1 is used for alpha, the k/v mapping comes from tui
-local function key_to_graphmode(k)
-	local tbl = {
-		primary = 2,
-		secondary = 3,
-		background = 4,
-		text = 5,
-		cursor = 6,
-		altcursor = 7,
-		highlight = 8,
-		label = 9,
-		warning = 10,
-		error = 11,
-		alert = 12,
-		inactive = 13
-	};
-	return tbl[k];
-end
-
-local function gen_palette_menu()
-	local list = {};
-	for i,v in ipairs(HC_PALETTE) do
-		local tbl = {
-			name = "hc_ind_" .. tostring(i),
-			format = HC_PALETTE[i],
-			label = "COLOR_" .. tostring(i)
-		};
-		suppl_append_color_menu(v, tbl, function(new)
-			HC_PALETTE[i] = new;
-		end);
-		table.insert(list, tbl);
-	end
-	return list;
-end
-
-local color_menu = {
-{
-	name = "hc_palette",
-	kind = "action",
-	label = "Palette",
-	submenu = true,
-	description = "High contrast palette, used for file browser and widgets",
-	handler = gen_palette_menu()
-},
-{
--- text color
--- label color
--- alertstr
--- labelstr
--- menulblstr
--- menulblselstr
--- helperstr
--- errstr
--- seltextstr
--- pretiletext_color
--- tbar_textstr
-}
-};
-
-local function apply_scheme(palette, wnd)
-	if (palette and valid_vid(wnd.external, TYPE_FRAMESERVER)) then
--- used to convey alpha, color scheme, etc. primarily for TUIs
-		for k,v in ipairs(palette) do
-			local ind = key_to_graphmode(k);
-			if (ind and type(v) == "table" and #v == 3) then
-				target_graphmode(wnd.external, ind, v[1], v[2], v[3]);
-			else
-				warning("apply_scheme(), broken key " .. k);
-			end
-		end
--- commit
-		target_graphmode(wnd.external, 0);
-	end
-end
-
 return {
 -- thickness is dependent on area, make sure the labels and
 -- constraints update dynamically
@@ -302,6 +104,14 @@ return {
 		handler = system_load("menus/global/shaders.lua")();
 	},
 	{
+		name = "animations",
+		label = "Animations",
+		kind = "action",
+		submenu = true,
+		description = "Control animation speed and effect",
+		handler = system_load("menus/global/animations.lua")();
+	},
+	{
 		name = "mouse_scale",
 		label = "Mouse Scale",
 		kind = "value",
@@ -314,70 +124,12 @@ return {
 		end
 	},
 	{
-		name = "anim_speed",
-		label = "Animation Speed",
-		kind = "value",
-		hint = "(1..100)",
-		description = "Change the animation speed used for UI elements",
-		validator = gen_valid_num(1, 100),
-		initial = function() return tostring(gconfig_get("animation")); end,
-		handler = function(ctx, val)
-			gconfig_set("animation", tonumber(val));
-		end
-	},
-	{
-		name = "trans_speed",
-		label = "Transition Speed",
-		kind = "value",
-		hint = "(1..100)",
-		description = "Change the animation speed used in state transitions",
-		validator = gen_valid_num(1, 100),
-		initial = function() return tostring(gconfig_get("transition")); end,
-		handler = function(ctx, val)
-			gconfig_set("transition", tonumber(val));
-		end
-	},
-	{
-		name = "wnd_speed",
-		label = "Window Animation Speed",
-		kind = "value",
-		hint = "(0..50)",
-		description = "Change the animation speed used with window position/size",
-		validator = gen_valid_num(0, 50),
-		initial = function() return tostring(gconfig_get("wnd_animation")); end,
-		handler = function(ctx, val)
-			gconfig_set("wnd_animation", tonumber(val));
-		end
-	},
-	{
-		name = "anim_in",
-		label = "Transition-In",
-		kind = "value",
-		description = "Change the effect used when moving a workspace on-screen",
-		set = {"none", "fade", "move-h", "move-v"},
-		initial = function() return tostring(gconfig_get("ws_transition_in")); end,
-		handler = function(ctx, val)
-			gconfig_set("ws_transition_in", val);
-		end
-	},
-	{
-		name = "anim_out",
-		label = "Transition-Out",
-		kind = "value",
-		description = "Change the effect used when moving a workspace off-screen",
-		set = {"none", "fade", "move-h", "move-v"},
-		initial = function() return tostring(gconfig_get("ws_transition_out")); end,
-		handler = function(ctx, val)
-			gconfig_set("ws_transition_out", val);
-		end
-	},
-	{
 		name = "colors",
 		label = "Colors",
 		description = "Special colors that are not shader- defined or decorations",
 		kind = "action",
 		submenu = true,
-		handler = color_menu
+		handler = system_load("menus/global/colors.lua")
 	},
 	{
 		name = "menu_helper",
