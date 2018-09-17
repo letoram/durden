3a0e22254958d16bfec2ea592367f272ceb9c438
bjornstahl <arcanfe@nospam>
2018-09-18 00:53:14 +0200

    vrviewer video templates

---
+++ durden/tools/vrviewer/spaces/sphere-180.lua
@@ -0,0 +1,13 @@
+return {
+"layers/add=bg",
+"layers/layer_bg/settings/depth=50.0",
+"layers/layer_bg/settings/radius=50.0",
+"layers/layer_bg/settings/fixed=true",
+"layers/layer_bg/add_model/hemisphere=bg",
+"layers/layer_bg/models/bg/set_default",
+"layers/layer_bg/models/bg/flip=true"
+};
+
+-- "layers/layer_bg/add_model/sphere=bg",
+-- "layers/layer_bg/models/bg/source=center360.png",
+-- "layers/layer_bg/models/bg/connpoint/temporary=360bg",
