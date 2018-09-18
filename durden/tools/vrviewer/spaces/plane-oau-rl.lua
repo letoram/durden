2c57414db095026a5e46d56d7838ebce96b39053
arcanfe@nospam <arcanfe@nospam>
2018-09-18 02:49:39 +0000

    vrviewer video templates
    FossilOrigin-Name: 4b02acac101ba56d23ca78417563e8f0c0f2cd49f7fbce841af830a115238d94

---
+++ durden/tools/vrviewer/spaces/plane-oau-rl.lua
@@ -0,0 +1,13 @@
+return {
+"layers/add=fg",
+"layers/layer_fg/settings/active_scale=3",
+"layers/layer_fg/settings/inactive_scale=1",
+"layers/layer_fg/settings/depth=2.0",
+"layers/layer_fg/settings/radius=10.0",
+"layers/layer_fg/settings/spacing=0.0",
+"layers/layer_fg/settings/vspacing=0.1",
+"layers/layer_fg/add_model/rectangle=oaurl",
+"layers/layer_fg/models/oaurl/connpoint/reveal=oaurl",
+"layers/layer_fg/models/oaurl/stereoscopic=oau-rl",
+"layers/layer_fg/models/oaurl/set_default",
+};
---
+++ durden/tools/vrviewer/spaces/plane-sbs.lua
@@ -0,0 +1,13 @@
+return {
+"layers/add=fg",
+"layers/layer_fg/settings/active_scale=3",
+"layers/layer_fg/settings/inactive_scale=1",
+"layers/layer_fg/settings/depth=2.0",
+"layers/layer_fg/settings/radius=10.0",
+"layers/layer_fg/settings/spacing=0.0",
+"layers/layer_fg/settings/vspacing=0.1",
+"layers/layer_fg/add_model/rectangle=sbsvid",
+"layers/layer_fg/models/sbsvid/connpoint/reveal=planesbs",
+"layers/layer_fg/models/sbsvid/stereoscopic=sbs",
+"layers/layer_fg/models/sbsvid/set_default",
+};
