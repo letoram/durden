bc364dc8a5b39c278a9f18c2b6a9e3a93ec7c5a7
bjornstahl <arcanfe@nospam>
2019-06-27 22:29:18 +0200

    advfloat / resize-control cosmetic fixes

---
+++ durden/shaders/ui/rounded_border.lua
@@ -0,0 +1,56 @@
+return {
+	version = 1,
+	label = "Rounded Border",
+	filter = "none",
+	frag_source = "rounded_border.frag",
+	uniforms = {
+		radius = {
+			label = 'Radius',
+			utype = 'f',
+			default = 5,
+			description = 'Corner Radius Circle',
+			low = 1,
+			high = 20,
+		},
+		sigma = {
+			label = 'Sigma',
+			utype = 'f',
+			default = 2,
+			description = 'Shadow Blur Coefficient',
+			low = 1,
+			high = 20
+		},
+		weight = {
+			label = 'Weight',
+			utype = 'f',
+			default = 0.5,
+			description = 'Shadow Blur Weight',
+			low = 0.1,
+			high = 1.0
+		},
+		shadow_color = {
+			label = "Shadow",
+			utype = 'fff',
+			default = {0.05, 0.05, 0.05},
+			low = 0.0,
+			high = 1.0,
+			description = 'Base shadow color to use',
+		},
+		border_color = {
+			label = "Border",
+			utype = 'fff',
+			default = {0.7, 0.7, 0.7},
+			low = 0.0,
+			high = 1.0,
+			description = "Border color to use"
+		},
+		border_thickness = {
+			label = "Thickness",
+			utype = 'f',
+			default = {0.04},
+			low = 0.01,
+			high = 0.1,
+			description = "Range assigned for border",
+		},
+	}
+};
--- durden/tools/advfloat/rzctl.lua
+++ durden/tools/advfloat/rzctl.lua
@@ -4,14 +4,16 @@ return function()
 		return;
 	end
 
+	local x, y = mouse_xy();
 -- warp the mouse to use that as the positioning
 	mouse_absinput_masked(
 		wnd.x + wnd.pad_left, wnd.y + wnd.pad_top, true);
 
+	local r, g, b = suppl_hexstr_to_rgb(HC_PALETTE[1]);
 	suppl_region_select(
-		128, 168, 128,
+		r, g, b,
 		function(x1, y1, x2, y2)
--- protect against tocu
+-- protect against toctu
 			if (not wnd or not wnd.move) then
 				return;
 			end
@@ -23,6 +25,7 @@ return function()
 			local w = x2 - x1;
 			local h = y2 - y1;
 			wnd:resize(w, h);
+			mouse_absinput_masked(x, y, true);
 		end
 	);
 end
