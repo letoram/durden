e44c925b5e2005f9dddbbb5406af49b8c0d6d0e0
arcanfe@nospam <arcanfe@nospam>
2020-01-05 15:10:03 +0000

    cli-popup contents adjustment
    add option to spawn terminal group and system controls.
    
    FossilOrigin-Name: ff0947d0e1c66ca392fb454f01c6997e73faa36bc32b525cfa824e83777b2cdd

--- durden/devmaps/menus/cli_icon.lua
+++ durden/devmaps/menus/cli_icon.lua
@@ -1,6 +1,7 @@
 -- this menu is applied as a context menu to the workspace- buttons in the statusbar
 return {
+{"CLI Group", "/global/open/terminal_group"},
 {"Open", "/global/open/target"},
-{"System", "/system"},
+{"System", "/global/system"},
 {"Settings", "/global/settings"}
 };
