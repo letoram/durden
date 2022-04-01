1ff353de1050b330522df5eb1f348a48a2824780
arcanfe@nospam <arcanfe@nospam>
2022-04-01 01:01:40 +0000

    simplify clipboard management
    FossilOrigin-Name: 1451df43856fe9dba666a41dfd7e235c7dbbdf29036535624628b18b839fa0b3

--- durden/atypes/clipboard.lua
+++ durden/atypes/clipboard.lua
@@ -3,78 +3,75 @@
 -- artificially restricted to one active clipboard manager.
 --
 
+local log, fmt = suppl_add_logfn("clipboard");
 local cm = nil;
+
 local function clipboard_event(ctx, msg, source)
 	if (not cm or source == cm) then
 		return;
 	end
 
-	if (msg and string.len(msg) > 0 and
-		valid_vid(cm, TYPE_FRAMESERVER)) then
+	if (msg and string.len(msg) > 0 and valid_vid(cm, TYPE_FRAMESERVER)) then
 		local mode = gconfig_get("clipboard_access");
 		if (mode == "passive" or mode == "full") then
-			message_target(cm, msg);
+			message_target(cm.vid, msg);
 		end
 	end
 end
 
 CLIPBOARD:set_monitor(clipboard_event);
 
+local clipboard_dispatch =
+{
+	segment_request =
+	function(...)
+	end,
+
+	terminated =
+	function(wnd, ...)
+		CLIPBOARD:set_provider(wnd)
+		if cm == wnd then
+			cm = nil
+		end
+		return false
+	end,
+
+	message =
+	function(wnd, source, stat)
+		local mode = gconfig_get("clipboard_access");
+		if (mode == "active" or mode == "full") then
+			CLIPBOARD:add(source, stat.message, stat.multipart);
+		else
+			log("rejected:reason=external_copy_blocked")
+		end
+	end,
+
+	registered =
+	function(wnd, source, stat)
+		if cm then
+			log("collision:message=got existing clipboard manager")
+			wnd:destroy();
+			return;
+		end
+
+		if gconfig_get("clipboard_access") == "none" then
+			log("rejected:reason=external_blocked")
+			wnd:destroy()
+			return
+		end
+
+		log("accepted:kind=external_manager")
+		cm = wnd
+	end
+}
+
 return {
 	atype = "clipboard",
 	default_shader = {"simple", "noalpha"},
 	actions = {},
-	props = {},
-
--- Intercept creation so that the window won't be made visible
--- (as the ws_attach() function will never be called)
-	intercept = function(ctx, wnd, source, tbl)
-		if (gconfig_get("clipboard_access") == "none") then
-			active_display():message(
-				"rejected clipboard injection/monitor connection attempt");
-			return false;
-		end
-
--- patch in a nonsense source so that we retain control over [source]
--- and can safely just return false and have the window destroy.
-		if (valid_vid(cm)) then
-			delete_image(cm);
-			cm = nil;
-		end
-		wnd.vid = null_surface(1, 1);
-		wnd.external = nil;
-		if (valid_vid(wnd.vid)) then
-			link_image(wnd.vid, wnd.anchor);
-		end
-		link_image(source, WORLDID);
-		cm = source;
-
--- and set a dumb handler that only takes message/termination requests
--- into account, this does not setup a pasteboard for a/v operation and
--- copy- operations (stepframe, resized, ...) is also not used.
-		target_updatehandler(source,
-			function(src, stat)
-				if (stat.kind == "terminated") then
-					delete_image(source);
-				elseif (stat.kind == "message") then
-					local mode = gconfig_get("clipboard_access");
-					if (mode == "active" or mode == "full") then
-						CLIPBOARD:add(source, stat.message, stat.multipart);
-					end
-				end
-			end
-		)
-
--- here is a decent place to add the support for flushing the current global
--- clipboard history, though it is not particularly easy to actually add since
--- the storm of long, possibly multipart_ messages may not fit the event-queue
--- of the target, so the message_target call result would have to be treated
--- as a 'short_write' and retry.
-
--- will cause the wnd to be destroy, but we've already take control over source
-		return false;
-	end,
--- won't be used
-	dispatch = {
-	}
+	props =
+	{
+		attach_block = true
+	},
+	dispatch = clipboard_dispatch
 };
