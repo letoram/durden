3e4d8f53849e41430c08b2cce8029b305e981007
arcanfe@nospam <arcanfe@nospam>
2023-08-11 17:08:32 +0000

    tool/a12 - start adding button and discovery ctrls
    This is a bit weird to develop as it hinges on changes throughout
    the stack as well as experimental durden branches with unmerged
    code.
    
    This adds the basics for controlling buttons added to the statusbar
    when signalling changes to a discovered directory server or known
    sources. (sinks come with virtual displays and should integrate in
    the other sharing menus, including 'sinking' to a directory').
    
    The real kicker for the coming release is to get dynamic updates for an
    established directory server when there is a new source or appl
    available, or reacting when an appl that is running has changed, with
    the default being state-store + reload + state-restore.
    
    The later would allow a live-coding scenario where someone with access
    could push an appl or changes to a shared directory, and others using it
    immediately update, with crash/bug collection and
    revert-to-last-working.
    
    FossilOrigin-Name: d26bc42cf1c737c55fd589b908438bb1aa5ed42566b5d99e75134bd8b3f1550b

--- durden/tools/a12net.lua
+++ durden/tools/a12net.lua
@@ -5,7 +5,6 @@ local log, fmt = suppl_add_logfn("tools");
 local active_dir = {};
 local active_kind = {};
 
-local known = {};
 local scanner_active = false;
 local trust_mode = TRUST_KNOWN;
 
@@ -15,6 +14,9 @@ local function gen_dhandler(mode)
 		if status.kind == "terminated" then
 			log(fmt("discover:mode=%s:terminated:reason=%s", mode, status.last_words));
 			delete_image(source);
+			if active_kind[mode] and active_kind[mode].button then
+				active_kind[mode].button:destroy();
+			end
 			active_kind[mode] = nil;
 		elseif kind ~= "status" then
 			return;
@@ -22,13 +24,42 @@ local function gen_dhandler(mode)
 	end
 end
 
+local function button_factory(side, label, path, altpath)
+	local side = side == "button_left" and "left" or "right";
+	local wm = active_display();
+	local bar = wm:get_dock();
+	local sbh = math.clamp(
+		math.ceil(gconfig_get("sbar_sz") * wm.scalef),
+		math.ceil(gconfig_get("sbar_min_sz") * wm.scalef)
+	);
+	local res =
+		bar:add_button(side,
+				"sbar_item_bg", "sbar_item",
+				label,
+				gconfig_get("sbar_tpad") * wm.scalef,
+				wm.font_resfn,
+				nil, nil,
+				mouse_handler_factory.statusbar_icon(wm, path, altpath)
+			);
+	wm:tile_update();
+	return res;
+end
+
 local function add_discover(mode, vid)
 	if not valid_vid(vid) then
 		log("discover:deep:launch fail");
 		return;
 	end
+
+	local tbl = {vid = vid};
+	local dmode = gconfig_get("a12net_on_update");
+
+	if dmode == "button_left" or dmode == "button_right" then
+		button_factory(dmode, mode, "", "");
+	end
+
 	target_flags(vid, TARGET_BLOCKADOPT);
-	active_kind[mode] = vid;
+	active_kind[mode] = tbl;
 end
 
 local function launch_appl(v, id)
@@ -92,8 +123,85 @@ local function gen_a12dir_active()
 	return res;
 end
 
-local function gen_stop_menu()
-	local keys = {"passive", "sweep", "brodcast", "test", "dir_src", "dir_sink"};
+local function gen_menu_for_known(ent, ind)
+-- might be a multipart in progress
+	if not ent.complete then
+		return;
+	end
+
+-- preferred order:
+	local names = {};
+	local res = {};
+
+	for _,v in ipairs({"tag", "ipv4", "ipv6", "dns", "a12pub"}) do
+		if ent.names[v] then
+			table.insert(names, v);
+		end
+	end
+
+-- only consider sources and directories
+	if not ent.source and not ent.directory then
+		return;
+	end
+
+	if #names == 0 then
+		log("kind=warning:source=a12net_discover:bad_entry");
+		return;
+	end
+
+-- source / directory are still possible bitmasks, but directory takes priority
+	local npref = (ent.source and "source_" or "") .. (ent.directory and "dir_" or "");
+	local lpref = (ent.source and "Source:" or "") .. (ent.directory and "Directory:" or "");
+
+	res.name = npref .. tostring(ind);
+	res.description = table.concat(names, " / ");
+	res.kind = "action";
+	res.label = lpref .. names[i];
+
+	if ent.directory then
+	res.handler =
+		function()
+
+		end
+	else
+
+	end
+
+	return res;
+end
+
+local function gen_discover_menu(v)
+	local res =
+	{
+		{
+			name = "stop",
+			label = "Stop",
+			kind = "action",
+			handler = function()
+				if not active_kind[v] then
+					return;
+				end
+				delete_image(active_kind[v].vid);
+				if active_kind[v].button then
+					active_kind[v].button:destroy();
+				end
+				active_kind[v] = nil;
+			end
+		}
+	};
+
+	for i,v in ipairs(v.known) do
+		local item = gen_menu_for_known(v, i);
+		if item then
+			table.insert(res, item);
+		end
+	end
+
+	return res;
+end
+
+local function get_active_menu()
+	local keys = {"passive", "sweep", "brodcast", "test"};
 	local res = {};
 	for _,v in ipairs(keys) do
 		if active_kind[v] then
@@ -102,18 +210,14 @@ local function gen_stop_menu()
 					name = v,
 					kind = "action",
 					label = v,
-					description = "Stop " .. v .. " discovery",
-					handler = function()
-						if not valid_vid(active_kind[v]) then
-							return;
-						end
-						delete_image(active_kind[v]);
-						active_kind[v] = nil;
-					end,
+					submenu = true,
+					description = "Stop or view " .. v .. " discover process",
+					handler = gen_discover_menu(v)
 				}
 			);
 		end
 	end
+
 	return res;
 end
 
@@ -125,6 +229,9 @@ local function discover_menu()
 			kind = "action",
 			label = "Passive",
 			description = "Listen on the local network for known beacons",
+			eval = function()
+				return active_kind["passive"] == nil;
+			end,
 			handler = function()
 				add_discover("passive",
 					net_discover(DISCOVER_PASSIVE, trust_model, gen_dhandler("passive")));
@@ -135,6 +242,9 @@ local function discover_menu()
 			kind = "action",
 			label = "Sweep",
 			description = "Periodically ping all tags in the keystore",
+			eval = function()
+				return active_kind["sweep"] == nil;
+			end,
 			handler = function()
 				add_discover("sweep",
 					net_discover(DISCOVER_SWEEP, trust_model, gen_dhandler("sweep")));
@@ -145,48 +255,27 @@ local function discover_menu()
 			kind = "action",
 			label = "Broadcast",
 			description = "Transmit a challenge beacon in the local broadcast domain",
+			eval = function()
+				return active_kind["broadcast"] == nil;
+			end,
 			handler = function()
 				add_discover("broadcast",
 					net_discover(DISCOVER_BROADCAST, trust_model, gen_dhandler("broadcast")));
 			end,
 		},
 		{
-			name = "dir_src",
-			kind = "value",
-			label = "Directory-Source",
-			description = "Become discoverable as a source through a third party",
-			validator = a12_dirstr,
-			handler = function(ctx, val)
-				add_discover("dir_src",
-					net_discover(DISCOVER_DIRECTORY,
-						trust_model, "mode=source:" .. val, gen_dhandler("dir_source")));
-			end
-		},
-		{
-			name = "dir_sink",
-			kind = "value",
-			label = "Directory-Sink",
-			description = "Become discoverable as a source through a third party",
-			validator = a12_dirstr,
-			handler = function(ctx, val)
-				add_discover("dir_sink",
-					net_discover(DISCOVER_DIRECTORY,
-						trust_model, "mode=sink:" .. val, gen_dhandler("dir_sink")));
-			end
-		},
-		{
-			name = "stop",
+			name = "active",
 			kind = "action",
-			label = "Stop",
-			description = "Stop ongoing discovery process",
+			label = "Active",
+			description = "Control / View an ongoing discovery process",
 			eval = function()
-				for _, v in pairs(active_kind) do
+				for _,v in pairs(active_kind) do
 					return true;
 				end
 			end,
 			submenu = true,
-			handler = gen_stop_menu
-		}
+			handler = get_active_menu
+		},
 	};
 
 	if DEBUGLEVEL > 1 and DISCOVER_TEST ~= nil then
@@ -195,6 +284,9 @@ local function discover_menu()
 			name = "test",
 			kind = "action",
 			label = "Test",
+			eval = function()
+				return active_kind["test"] == nil;
+			end,
 			description = "Test discovery type which sends both valid and broken events at increasing intervals",
 			handler = function()
 				add_discover("test", net_discover(DISCOVER_TEST, dhandler));
@@ -240,46 +332,62 @@ menus_register("target", "share", {
 }
 )
 
--- enforice [a12net_on_initial, a12net_on_update] triggers for when a
+-- enforce [a12net_on_initial, a12net_on_update] triggers for when a
 -- directory server connection is established.
-local function dir_list_trigger(dir, key)
+local function dir_list_trigger(status, dir, key, host)
 	if dir.known then
+		local mode = gconfig_get("a12net_on_update");
+		if mode == "alert" then
+		elseif mode == "notify" then
+
+		end
 		return;
 	end
 
--- button is slightly more annoying, we lack an indirection for keeping
--- track over when
 	local mode = gconfig_get("a12net_on_initial");
+	local popup_path =
+		"/global/tools/popup/menu=/global/open/a12/active/ent_" .. tostring(key);
+	local delta_path =
+		"/global/tools/popup/menu=/global/open/a12/new/ent_" .. tostring(key);
+
 	if mode == "button_left" or mode == "button_right" then
+-- add the button and map to the same popup
+		dir.button = button_factory(side, host, popup_path, delta_path);
+		dir.button:switch_state("alert");
 
 	elseif mode == "popup" then
-		dispatch_symbol("/global/tools/popup/menu=/global/open/a12/active/ent_" .. tostring(key));
+		dispatch_symbol(popup_path);
 	elseif mode == "menu" then
 		dispatch_symbol("/global/open/a12/active/ent_" .. tostring(key));
 	end
 
 	dir.closure =
-	function()
+	function(self)
+		if self.button then
+			self.button:destroy();
+		end
 	end
 
 	dir.known = true;
 end
 
-local function get_dir_cbh(key, dtbl)
+local function get_dir_cbh(key, host)
 	return
 	function(source, status)
 		if status.kind == "terminated" then
 			delete_image(source);
+
 			if active_dir[source].closure then
-				active_dir[source].closure();
+				active_dir[source]:closure();
 			end
 			active_dir[source] = nil;
 
 --
--- Should track launch / expected, the other special thing here is that state
--- management is split in two, arcan-net/afsrv_net handles it in one way, but
--- if the directory server dies things we need a better strategy, including
--- some way to re-establish the connection.
+-- Should track launch / expected, as well as progress on the transfer, the
+-- other special thing here is that state management is split in two,
+-- arcan-net/afsrv_net handles it in one way, but if the directory server dies
+-- things we need a better strategy, including some way to re-establish the
+-- connection.
 --
 -- The most likely solution would be to push a monitor socketpair as
 -- bchunkevents and then forget about it ourselves.
@@ -291,6 +399,19 @@ local function get_dir_cbh(key, dtbl)
 					durden_launch(hover, "", "external", nil, {});
 				end
 			end
+
+		elseif status.kind == "discovered" then
+
+-- there is a new or lost relative directory, source or sink
+--
+--       this needs to convey connection options
+--       and we need to tell the directory service what we want (relay, natpunch, direct)
+--       then get the connection data in return
+--
+		elseif status.kind == "streamstatus" then
+
+-- completion progress of appl-download (relevant until segment_request)
+--  show as mouse cursor, button progress, ...
 
 		elseif status.kind == "bchunkstate" then
 			local tbl = string.split(status.extensions, ";");
@@ -305,8 +426,14 @@ local function get_dir_cbh(key, dtbl)
 				end
 			end
 
+-- don't run triggers when appl is specified
 			if not status.multipart then
-				dir_list_trigger(active_dir[source], source);
+				if key then
+-- the appl we were looking for wasn't there, notify and leave
+					delete_image(source);
+				else
+					dir_list_trigger(status, active_dir[source], source, host);
+				end
 			end
 
 		else
@@ -331,7 +458,7 @@ local function gen_a12dir()
 					return;
 				end
 
-				local vid = net_open(val, get_dir_cbh(nil));
+				local vid = net_open(val, get_dir_cbh(nil, val));
 				if valid_vid(vid) then
 					target_flags(vid, TARGET_BLOCKADOPT);
 					image_tracetag(vid, "net_discover");
@@ -377,17 +504,29 @@ local network_opts = {
 		label = "List Updated",
 		initial = function() return gconfig_get("a12net_on_update"); end,
 		description = "Set the default action for when there are changes to the directory.",
-		set = {"alert", "event", "ignore"},
+		set = {"alert", "notify", "ignore"},
 		handler = function(ctx, val)
 			gconfig_set("a12net_on_update", val);
+		end
+	},
+	{
+		kind = "value",
+		name = "discover",
+		label = "On Discover",
+		initial = function() return gconfig_get("a12net_on_discover"); end,
+		description = "Set the default action for discovery processes.",
+		set = {"button_left", "button_right", "notify"},
+		handler = function(ctx, val)
+			gconfig_get("a12net_on_discover", val);
 		end
 	}
 };
 
 gconfig_register("a12net_on_initial", "button_left");
+gconfig_register("a12net_on_discover", "button_left");
 gconfig_register("a12net_on_update", "alert");
 
-menus_register("global", "settings",
+menus_register("global", "settings/tools",
 {
 	kind = "action",
 	name = "networking",
