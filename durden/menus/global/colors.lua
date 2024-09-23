84195cfdfbdc3456c07e27a0eab649168456aa03
void <>
2024-09-23 01:12:25 +0000

    expose option to pick high-contrast palette preset
    FossilOrigin-Name: e597b79b0b79454cf36d906cfcd0f6f77b5364a76817c0e90652f756ebb713e5

--- durden/menus/global/colors.lua
+++ durden/menus/global/colors.lua
@@ -17,8 +17,111 @@ local function key_to_graphmode(k)
 	return tbl[k];
 end
 
+local preset_set =
+{
+	monochrome = {
+		"\\#a0a0a0",
+		"\\#afafaf",
+		"\\#bababa",
+		"\\#bfbfbf",
+		"\\#cacaca",
+		"\\#cfcfcf",
+		"\\#dadada",
+		"\\#dfdfdf"
+	},
+	default = {
+		"\\#efd469",
+		"\\#43abc9",
+		"\\#cd594a",
+		"\\#b5c689",
+		"\\#f58b4c",
+		"\\#ed6785",
+		"\\#d0d0d0",
+	},
+	pastel = {
+		"\\#ffadad",
+		"\\#ffd6a5",
+		"\\#fdffb6",
+		"\\#caffbf",
+		"\\#9bf6ff",
+		"\\#a0c4ff",
+		"\\#bdb2ff",
+		"\\#ffc6ff",
+	},
+	browns = {
+		"\\#a48971",
+		"\\#8d6b48",
+		"\\#9a774f",
+		"\\#a9845a",
+		"\\#be986d",
+		"\\#d2a87d",
+		"\\#e8c9ab"
+	},
+	light = {
+		"\\#f1e2d7",
+		"\\#f9f9f7",
+		"\\#f8d5c2",
+		"\\#deeae8",
+		"\\#cddcdf",
+		"\\#f3e7e3",
+		"\\#f8eeea",
+		"\\#f5d3c5"
+	},
+	aqua = {
+		"\\#00f0d0",
+		"\\#00ffc8",
+		"\\#00e2d8",
+		"\\#00d3e0",
+		"\\#00c5e7",
+		"\\#00b6ef",
+		"\\#00a8f7",
+		"\\#0099ff"
+	},
+	dull = {
+		"\\#cb997e",
+		"\\#eddcd2",
+		"\\#fff1e6",
+		"\\#f0efeb",
+		"\\#ddbea9",
+		"\\#a5a58d",
+		"\\#b7b7a4",
+		"\\#c5c5b6"
+	}
+}
+
 local function gen_palette_menu()
-	local list = {};
+	local pc = {}
+	for k,v in pairs(preset_set) do
+		table.insert(pc, k)
+	end
+	table.sort(pc)
+
+	local list = {
+		{
+			name = "schemes",
+			kind = "value",
+			label = "Presets",
+			description = "Pick a preset high-contrast scheme",
+			set = pc,
+			handler = function(ctx, val)
+				local pal = preset_set[val]
+				HC_PALETTE = pal
+				local keys = {
+					"lbar_textstr",
+					"lbar_alertstr",
+					"lbar_labelstr",
+					"lbar_menulblstr",
+					"lbar_menulblselstr",
+					"lbar_helperstr",
+					"lbar_errstr"
+				}
+				for i,v in ipairs(keys) do
+					gconfig_set(v, HC_PALETTE[i] .. " ")
+				end
+			end,
+		}
+	};
+
 	for i,v in ipairs(HC_PALETTE) do
 		local tbl = {
 			name = "hc_ind_" .. tostring(i),
